9563f5b9f784a9bad63ae37a6ae7195540d2e84b
paulober <44974737+paulober@users.noreply.github.com>
2024-01-02 12:10:40 +0100

    Initial Commit

---
+++ CatchTheApple Watch App/CatchTheAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheAppleApp.swift
+//  CatchTheApple Watch App
+//
+//  Created by Paul on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheApple_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
